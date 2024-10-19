129e402269d2a7dcf610075e82a2dc8bf910baac
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-10-19 13:02:55 +0200

    Update gitignore

---
+++ dist/extension.js
@@ -0,0 +1,2 @@
+let activate2=()=>{console.log("EEE")};function activate(A){activate2(),A.readFile("/home/javier/Code/Tests/package.json").then(A=>{console.log("AAAAAAAAAAAAAAAAAAAAA")})}function deactivate(){}export{activate,deactivate};
+//# sourceMappingURL=extension.js.map
